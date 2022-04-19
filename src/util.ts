eb967768f8fd07c015cccb6e1f5a60b007d13c97
Andrea Nardi <buongiorno19972@gmail.com>
2022-04-19 19:53:22 +0200

    added visual narrative editor example

--- src/util.ts
+++
@@ -1,5 +0,0 @@
-import { ReadonlyMat4 } from 'gl-matrix';
-
-export function mat4ToTransformMatrix(m: ReadonlyMat4): string {
-  return `matrix3d(${m[0]},${m[1]},${m[2]},${m[3]},${m[4]},${m[5]},${m[6]},${m[7]},${m[8]},${m[9]},${m[10]},${m[11]},${m[12]},${m[13]},${m[14]},${m[15]})`;
-}
